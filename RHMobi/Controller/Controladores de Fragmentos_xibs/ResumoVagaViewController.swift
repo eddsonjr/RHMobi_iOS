62e7a3deb712092b3cf1e2a00a6e2d78b372cb13
Edson Jr <eds_jr@hotmail.com>
2018-03-13 15:21:31 -0400

    [ListaVagas] - Colocados os campos dentro do arquivo de xib do resmumo da vaga. Criando agora o de descricao da vaga.
    Signed-off-by: Edson Jr <eds_jr@hotmail.com>

---
+++ RHMobi/Controller/Controladores de Fragmentos_xibs/ResumoVagaViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ResumoVagaViewController.swift
+//  RHMobi
+//
+//  Created by Edson  Jr on 13/03/18.
+//  Copyright © 2018 Edson  Jr. All rights reserved.
+//
+
+import UIKit
+
+class ResumoVagaViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+  
+
+}
