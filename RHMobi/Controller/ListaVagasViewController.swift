73af6e45f346f55233c98b2f2afd37fe389bb4b9
Edson Jr <eds_jr@hotmail.com>
2018-03-12 22:13:48 -0400

    Busca parcialmente funcional
    Signed-off-by: Edson Jr <eds_jr@hotmail.com>

--- RHMobi/Controller/ListaVagasViewController.swift
+++ RHMobi/Controller/ListaVagasViewController.swift
@@ -9,14 +9,19 @@
 import UIKit
 
 
-class ListaVagasViewController: UIViewController,JsonDelegate,UITableViewDelegate,UITableViewDataSource {
+class ListaVagasViewController: UIViewController,JsonDelegate,UITableViewDelegate,UITableViewDataSource,UISearchBarDelegate {
     
     //Mark: Atributos visuais
     @IBOutlet weak var vagasTableView: UITableView!
-    
+    @IBOutlet weak var vagasSearchBar: UISearchBar!
     
     //Mark: Lista de variaveis
     var listaVagas = [Vaga]()
+    var procurando = false //Controla se a pessoa esta pesquisando
+    var dadosFiltradosBusca = [Vaga]()
+    
+    
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -25,6 +30,10 @@ class ListaVagasViewController: UIViewController,JsonDelegate,UITableViewDelegat
         //Configurando o delegate e o datasource da tableView
         self.vagasTableView.delegate = self
         self.vagasTableView.dataSource = self
+        
+        //Configurando o delegate da search bar
+        self.vagasSearchBar.delegate = self
+        self.vagasSearchBar.returnKeyType = UIReturnKeyType.done
         
         //Fazendo download do json
         decode(jsonUrl: UrlEnumHelper.vagasDownlodUrl.rawValue) {
@@ -87,6 +96,11 @@ class ListaVagasViewController: UIViewController,JsonDelegate,UITableViewDelegat
     
     //Mark: Funcoes para conformar com os protocolos da tableView
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        if self.procurando {
+            return self.dadosFiltradosBusca.count
+        }
+        
         return self.listaVagas.count
     }
 
@@ -94,14 +108,29 @@ class ListaVagasViewController: UIViewController,JsonDelegate,UITableViewDelegat
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "VagasCell") as? VagasTableViewCell
         
-        //Acessando os atributos da celula
-        cell?.vagaNomeLabel.text = self.listaVagas[indexPath.row].nome
-        cell?.vagaDataSubmissao.text = self.listaVagas[indexPath.row].prazo
-        cell?.vagaAreaInteresseLabel.text = self.listaVagas[indexPath.row].cargo
         
+        if procurando {
+            //Acessando os atributos da celula
+            cell?.vagaNomeLabel.text = self.dadosFiltradosBusca[indexPath.row].nome
+            cell?.vagaDataSubmissao.text = self.dadosFiltradosBusca[indexPath.row].prazo
+            cell?.vagaAreaInteresseLabel.text = self.dadosFiltradosBusca[indexPath.row].cargo
+            
+            //Fazendo download da imagem de forma assincrona
+            cell?.vagaImageView.downloadedFrom(link: self.dadosFiltradosBusca[indexPath.row].imgUrl)
+            
+        }else {
+            //Acessando os atributos da celula
+            cell?.vagaNomeLabel.text = self.listaVagas[indexPath.row].nome
+            cell?.vagaDataSubmissao.text = self.listaVagas[indexPath.row].prazo
+            cell?.vagaAreaInteresseLabel.text = self.listaVagas[indexPath.row].cargo
+            
+            
+            //Fazendo download da imagem de forma assincrona
+            cell?.vagaImageView.downloadedFrom(link: self.listaVagas[indexPath.row].imgUrl)
+            
+        }
         
-        //Fazendo download da imagem de forma assincrona
-        cell?.vagaImageView.downloadedFrom(link: self.listaVagas[indexPath.row].imgUrl)
+       
         
         return cell!
         
@@ -117,6 +146,31 @@ class ListaVagasViewController: UIViewController,JsonDelegate,UITableViewDelegat
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         return 150
     }
+    
+    
+    //Funcao para realizar busca
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        
+        
+        print("Procurando...")
+        if(vagasSearchBar.text == nil || vagasSearchBar.text == "") {
+                self.procurando = false
+                view.endEditing(true)
+                self.vagasTableView.reloadData()
+        }else {
+            procurando = true
+            for vaga in listaVagas{
+                if(vaga.nome == self.vagasSearchBar.text){
+                    print("Achado: \(vaga.nome)")
+                    self.dadosFiltradosBusca.append(vaga)
+                }
+              self.vagasTableView.reloadData()
+              
+            }
+            
+        }
+    }
+    
 
 }
 
---
+++ RHMobi/Extensions/String+extension+IdentifySubstrings.swift
@@ -0,0 +1,19 @@
+//
+//  String+extension+IdentifySubstrings.swift
+//  RHMobi
+//
+//  Created by Edson  Jr on 12/03/18.
+//  Copyright © 2018 Edson  Jr. All rights reserved.
+//
+
+import Foundation
+
+
+extension String {
+    func contains(find: String) -> Bool{
+        return self.range(of: find) != nil
+    }
+    func containsIgnoringCase(find: String) -> Bool{
+        return self.range(of: find, options: .caseInsensitive) != nil
+    }
+}
