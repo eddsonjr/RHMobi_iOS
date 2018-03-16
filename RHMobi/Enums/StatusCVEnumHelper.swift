bc27e2cae57c3158191c192e0227a8b30bcd981d
Edson Jr <eds_jr@hotmail.com>
2018-03-16 17:08:25 -0400

    Criadas as classes de StatuscvenumHelper e StatusVagaEnumHelper.
    Signed-off-by: Edson Jr <eds_jr@hotmail.com>

---
+++ RHMobi/Enums/StatusCVEnumHelper.swift
@@ -0,0 +1,16 @@
+//
+//  StatusCVEnumHelper.swift
+//  RHMobi
+//
+//  Created by Edson  Jr on 16/03/18.
+//  Copyright © 2018 Edson  Jr. All rights reserved.
+//
+
+import Foundation
+
+enum StatusVCEnumHelper: String{
+    case enviado = "enviado"
+    case emAnalise = "emAnalise"
+    case aprovado = "aprovado"
+    case naoAprovado = "naoAprovado"
+}
---
+++ RHMobi/Enums/StatusVagaEnumHelper.swift
@@ -0,0 +1,17 @@
+//
+//  StatusVagaEnumHelper.swift
+//  RHMobi
+//
+//  Created by Edson  Jr on 16/03/18.
+//  Copyright © 2018 Edson  Jr. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+enum StatusVagaEnumHelper: String {
+    case indisponivel = "indisponivel"
+    case emAberto = "emAberto"
+    case cancelada = "cancelada"
+    case preenchida = "preenchida"
+}
