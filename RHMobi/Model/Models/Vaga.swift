2fc605e8b42c0de29a44b215578c6d24ef5332f1
Edson Jr <eds_jr@hotmail.com>
2018-03-31 15:18:14 -0400

    [ListaVagas] - Criada a classe Vaga que conforma com o protocolo NSManagedObject. Criando agora as demais entidades de banco e posteriormente configurando suas respectivas classes.
    Signed-off-by: Edson Jr <eds_jr@hotmail.com>

--- RHMobi/Model/Models/Vaga.swift
+++ RHMobi/Model/Models/Vaga.swift
@@ -7,27 +7,36 @@
 //
 
 import Foundation
+import CoreData
+import SwiftyJSON
 
 
-class Vaga: Decodable,Encodable {
+
+class Vaga: NSManagedObject {
     
-    var id: Int
-    var nome: String
-    var tipoContrato: String
-    var funcao: String
-    var experiencia: String
-    var prazo: String //Mudar posteriormente para um tipo de dados de data
-    var descricao: String
-    var requisitos: [Requisitos]
-    var areasInteresse: [AreasInteresse]
-    var imgUrl: String
-    var vagaStatus: StatusVaga
-    var cliente: Cliente
+    @ NSManaged var id: Int
+    @ NSManaged var nome: String
+    @ NSManaged var tipoContrato: String
+    @ NSManaged var funcao: String
+    @ NSManaged var experiencia: String
+    @ NSManaged var prazo: String //Mudar posteriormente para um tipo de dados de data
+    @ NSManaged var descricao: String
+    @ NSManaged var requisitos: Set<Requisitos>
+    @ NSManaged var areasInteresse: Set<AreasInteresse>
+    @ NSManaged var imgUrl: String
+    @ NSManaged var vagaStatus: StatusVaga
+    @ NSManaged var cliente: Cliente
     
     
+    //Construtores
     
+    //Construtor completo
     init(id: Int, nome: String, tipoContrato: String, funcao: String, experiencia: String,
          prazo: String, descricao: String, requisitos: [Requisitos], areasInteresse: [AreasInteresse], imgUrl: String, vagaStatus: StatusVaga, cliente: Cliente) {
+        
+        let entity = NSEntityDescription.entity(forEntityName: "Vaga", in: CoreDataHelper.getContext())!
+        super.init(entity: entity, insertInto: CoreDataHelper.getContext())
+        
         
         self.id = id
         self.nome = nome
@@ -42,6 +51,33 @@ class Vaga: Decodable,Encodable {
         self.vagaStatus = vagaStatus
         self.cliente = cliente
         
+    }
+    
+    
+    //Construtor simples sem o relacionamento com as outras entidades
+    init(id: Int, nome: String, tipoContrato: String, funcao: String, experiencia: String,
+         prazo: String, descricao: String,imgUrl: String) {
+        
+        let entity = NSEntityDescription.entity(forEntityName: "Vaga", in: CoreDataHelper.getContext())!
+        super.init(entity: entity, insertInto: CoreDataHelper.getContext())
+        
+        
+        self.id = id
+        self.nome = nome
+        self.tipoContrato = tipoContrato
+        self.funcao = funcao
+        self.experiencia = experiencia
+        self.prazo = prazo
+        self.descricao = descricao
+        self.imgUrl = imgUrl
+        
+    }
+    
+
+    
+    @objc //Obrigado a colocar esse init para nao dar erro com o protocolo de NSObjectmanaged
+    private override init(entity: NSEntityDescription, insertInto context: NSManagedObjectContext?) {
+        super.init(entity: entity, insertInto: context)
     }
     
     
