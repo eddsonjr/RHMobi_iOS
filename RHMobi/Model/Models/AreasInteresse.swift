6058a26a04516588cb8f299965af73952e188a3f
Edson Jr <eds_jr@hotmail.com>
2018-03-31 15:47:56 -0400

    [ListaVagas] - Criado o relacionamento entre Vaga e AreasInteresse e tambem feito ajustes na classe de modelo para conformr com o protocolo do CoreData.
    Signed-off-by: Edson Jr <eds_jr@hotmail.com>

--- RHMobi/Model/Models/AreasInteresse.swift
+++ RHMobi/Model/Models/AreasInteresse.swift
@@ -7,19 +7,31 @@
 //
 
 import Foundation
+import CoreData
 
 
-class AreasInteresse: Decodable,Encodable {
+
+class AreasInteresse: NSManagedObjectContext {
     
-    var id: Int
-    var nome: String
+    @NSManaged var id: Int
+    @NSManaged var nome: String
     
+    
+    //Construtores
     init(id: Int, nome: String) {
+        let entity = NSEntityDescription.entity(forEntityName: "AreasInteresse", in: CoreDataHelper.getContext())!
+        super.init(entity: entity, insertInto: CoreDataHelper.getContext())
+       
         self.id = id
         self.nome = nome
     }
     
     
+    
+    @objc //Obrigado a colocar esse init para nao dar erro com o protocolo de NSObjectmanaged
+    private override init(entity: NSEntityDescription, insertInto context: NSManagedObjectContext?) {
+        super.init(entity: entity, insertInto: context)
+    }
     
     
     
