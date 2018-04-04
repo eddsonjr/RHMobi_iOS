09517689eebdc59dd830b2f5928bf98942c4ea96
Edson Jr <eds_jr@hotmail.com>
2018-04-04 15:14:11 -0400

    [Cadastro] - Cadastro sendo feito. Falta redirecionar para a pagina de login agora.
    Signed-off-by: Edson Jr <eds_jr@hotmail.com>

--- RHMobi/Controller/CadastroViewController.swift
+++ RHMobi/Controller/CadastroViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class CadastroViewController: UIViewController {
+class CadastroViewController: UIViewController,UITextFieldDelegate {
     
     //Mark: Variaveis de elementos da tela
     @IBOutlet weak var nomeTextField: UITextField!
@@ -56,6 +56,19 @@ class CadastroViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        //Configurando
+        self.nomeTextField.delegate = self
+        self.sobreNomeTextField.delegate = self
+        self.rgTextField.delegate = self
+        self.telefoneTextField.delegate = self
+        self.telefone2TextField.delegate = self
+        self.emailTextField.delegate = self
+        self.senhaTextField.delegate = self
+        self.confirmarSenhaTextField.delegate = self
+        
+        
+        
+        
         self.fotoHelper = FotoHelper(viewController: self)
         
         NotificationCenter.default.addObserver(self, selector: #selector(CadastroViewController.trocarImagemCadastro), name: NSNotification.Name(rawValue: NotificationKeysEnumHelper.fotoPronta.rawValue), object: nil)
@@ -68,6 +81,13 @@ class CadastroViewController: UIViewController {
             print("Dados do candidato: \(a.nome) -- \(a.sobrenome) -- \(a.email) -- \(a.id)")
         }
         
+        
+        
+        //Teclado
+        NotificationCenter.default.addObserver(self, selector: #selector(CadastroViewController.keyboardWillShow(sender:)), name: NSNotification.Name.UIKeyboardWillShow, object: nil)
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(CadastroViewController.keyboardWillHide(sender:)), name: NSNotification.Name.UIKeyboardWillHide, object: nil)
+
 
        
 
@@ -82,8 +102,11 @@ class CadastroViewController: UIViewController {
     //Mark: Actions dos botoes e da segmented
     
     @IBAction func cadastrar(_ sender: Any) {
+        print(dbgmsg + "Pressionado o botao de cadastrar")
         if(adqurirDadosDosCamposEValidando()){
             cadastrarCandidato()
+        }else{
+            print(dbgmsg + "Erros encontrados durante o cadastro.")
         }
 
     }
@@ -149,10 +172,14 @@ class CadastroViewController: UIViewController {
         let nome = nomeTextField.text // Adiquirindo o nome
         if ((nome == "" || nome == nil)){ //Verifica se o campo nao foi preenchido
             trocarLabelEImagemDeValidacao(label: self.nomeLabelConfirmacao, image: nomeImageConfirmacao, status: 0)
+            print(dbgmsg + "nome errado!")
             podeCadastrar = false
         }else if (nome?.isAllDigits())! { //verificar se o campo foi preenchido de forma incorreta
             trocarLabelEImagemDeValidacao(label: self.nomeLabelConfirmacao, image: nomeImageConfirmacao, status: 1)
+             print(dbgmsg + "nome errado!")
             podeCadastrar = false
+        }else {
+            trocarLabelEImagemDeValidacao(label: self.nomeLabelConfirmacao, image: nomeImageConfirmacao, status: 2)
         }
         
         
@@ -161,10 +188,14 @@ class CadastroViewController: UIViewController {
         let sobrenome = sobreNomeTextField.text //Adquirindo o sobrenome
         if ((sobrenome == "" || sobrenome == nil)){ //Verifica se o campo nao foi preenchido
             trocarLabelEImagemDeValidacao(label: self.sobrenomeLabelConfirmacao, image: sobrenomeImageConfirmacao, status: 0)
+             print(dbgmsg + "sobrenome errado!")
             podeCadastrar = false
         }else if (sobrenome?.isAllDigits())! { //verificar se o campo foi preenchido de forma incorreta
             trocarLabelEImagemDeValidacao(label: self.sobrenomeLabelConfirmacao, image: sobrenomeImageConfirmacao, status: 1)
+              print(dbgmsg + "sobrenome errado!")
             podeCadastrar = false
+        }else {
+            trocarLabelEImagemDeValidacao(label: self.sobrenomeLabelConfirmacao, image: sobrenomeImageConfirmacao, status: 2)
         }
         
         
@@ -172,10 +203,16 @@ class CadastroViewController: UIViewController {
         let email = emailTextField.text
         if ((email == "" || email == nil)){ //Verifica se o campo nao foi preenchido
             trocarLabelEImagemDeValidacao(label: self.emailLabelConfirmacao, image: emailImageConfirmacao, status: 0)
+              print(dbgmsg + "email errado!")
             podeCadastrar = false
-        }else if (email?.isEmailValid())! { //verificar se o campo foi preenchido de forma incorreta
-            trocarLabelEImagemDeValidacao(label: self.emailLabelConfirmacao, image: emailImageConfirmacao, status: 1)
-            podeCadastrar = false
+        }
+//        else if (email?.isEmailValid())! { //verificar se o campo foi preenchido de forma incorreta
+//            trocarLabelEImagemDeValidacao(label: self.emailLabelConfirmacao, image: emailImageConfirmacao, status: 1)
+//             print(dbgmsg + "email errado!")
+//            podeCadastrar = false
+//        }
+        else {
+            trocarLabelEImagemDeValidacao(label: self.emailLabelConfirmacao, image: emailImageConfirmacao, status: 2)
         }
         
         
@@ -183,10 +220,14 @@ class CadastroViewController: UIViewController {
         let rg = rgTextField.text
         if ((rg == "" || rg == nil)){ //Verifica se o campo nao foi preenchido
             trocarLabelEImagemDeValidacao(label: self.rgLabelConfirmacao, image: rgImageConfirmacao, status: 0)
+             print(dbgmsg + "rg errado!")
             podeCadastrar = false
         }else if (!(rg?.isAllDigits())!) { //verificar se o campo foi preenchido de forma incorreta
             trocarLabelEImagemDeValidacao(label: self.rgLabelConfirmacao, image: rgImageConfirmacao, status: 1)
+             print(dbgmsg + "rg errado!")
             podeCadastrar = false
+        }else {
+            trocarLabelEImagemDeValidacao(label: self.rgLabelConfirmacao, image: rgImageConfirmacao, status: 2)
         }
         
         
@@ -195,10 +236,14 @@ class CadastroViewController: UIViewController {
         let telefone1 = telefoneTextField.text
         if ((telefone1 == "" || telefone1 == nil)){ //Verifica se o campo nao foi preenchido
             trocarLabelEImagemDeValidacao(label: self.telefone1LabelConfirmacao, image: telefone1ImageConfirmacao, status: 0)
+             print(dbgmsg + "telefone errado!")
             podeCadastrar = false
         }else if (!(telefone1?.isAllDigits())!) { //verificar se o campo foi preenchido de forma incorreta
             trocarLabelEImagemDeValidacao(label: self.telefone1LabelConfirmacao, image: telefone1ImageConfirmacao, status: 1)
+             print(dbgmsg + "telefone errado!")
             podeCadastrar = false
+        }else {
+            trocarLabelEImagemDeValidacao(label: self.telefone1LabelConfirmacao, image: telefone1ImageConfirmacao, status: 2)
         }
         
         
@@ -207,7 +252,10 @@ class CadastroViewController: UIViewController {
         let telefone2 = telefone2TextField.text
         if (!(telefone2?.isAllDigits())!) { //verificar se o campo foi preenchido de forma incorreta
             trocarLabelEImagemDeValidacao(label: self.telefone2LabelConfirmacao, image: telefone2ImageConfirmacao, status: 1)
+            print(dbgmsg + "telefone errado!")
             podeCadastrar = false
+        }else {
+            trocarLabelEImagemDeValidacao(label: self.telefone2LabelConfirmacao, image: telefone2ImageConfirmacao, status: 2)
         }
         
         
@@ -218,10 +266,14 @@ class CadastroViewController: UIViewController {
         
         if((senha1Campo == "" || senha1Campo == nil) || (senha2Campo == "" || senha2Campo == nil)){
             trocarLabelEImagemDeValidacao(label: self.confirmarSenhaLabelConfirmacao, image: confirmarSesnhaImageConfirmacao, status: 0)
+            print(dbgmsg + "senha errada!")
             podeCadastrar = false
         }else if (senha1Campo != senha2Campo){
             trocarLabelEImagemDeValidacao(label: self.confirmarSenhaLabelConfirmacao, image: confirmarSesnhaImageConfirmacao, status: 1)
+            print(dbgmsg + "senhas nao conferem!")
             podeCadastrar = false
+        }else {
+            trocarLabelEImagemDeValidacao(label: self.confirmarSenhaLabelConfirmacao, image: confirmarSesnhaImageConfirmacao, status: 2)
         }
         
         return podeCadastrar
@@ -233,6 +285,7 @@ class CadastroViewController: UIViewController {
     
     func cadastrarCandidato(){
         
+        print(dbgmsg + "Chamando a funcao para cadastrar")
         viewModel.cadastrar(id: 1, nome: self.nomeTextField.text!, sobrenome: self.sobreNomeTextField.text!, sexo: self.sexo, rg: self.rgTextField.text!, email: self.emailTextField.text!, senha: self.senhaTextField.text!, telefone1: self.telefoneTextField.text!, telefone2: self.telefone2TextField.text!)
     }
     
@@ -250,20 +303,43 @@ class CadastroViewController: UIViewController {
         switch status {
         case 0:
                 label.text = "Preencha"
+                label.textColor = UIColor(red: 227/256, green: 82/256, blue: 101/256, alpha: 1.0)
                 image.image = #imageLiteral(resourceName: "Botao atenção")
                 break
         case 1:
                 label.text = "Verifique"
+                label.textColor = UIColor(red: 233/256, green: 162/256, blue: 67/256, alpha: 1.0)
                 image.image = #imageLiteral(resourceName: "Botao errado")
                 break
             
         default:
                 label.text = "Confirmado"
+                label.textColor = UIColor(red: 53/256, green: 157/256, blue: 132/256, alpha: 1.0)
                 image.image = #imageLiteral(resourceName: "Botao confirmado")
         }
         
     }
     
+    
+    
+    
+    
+    @objc func keyboardWillShow(sender: NSNotification) {
+        self.view.frame.origin.y = -150 // Move view 150 points upward
+    }
+    
+    @objc func keyboardWillHide(sender: NSNotification) {
+        self.view.frame.origin.y = 0 // Move view to original position
+    }
+    
+    
+    
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        self.view.endEditing(true)
+        return false
+    }
+ 
     
     
 
