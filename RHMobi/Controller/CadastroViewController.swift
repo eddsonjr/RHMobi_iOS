9e3e6880662217c4db0f5de073f9e02abb8fc929
Edson Jr <eds_jr@hotmail.com>
2018-04-05 16:15:02 -0400

    [Cadastro] Fazendo a imagem ficar arredondada.
    Signed-off-by: Edson Jr <eds_jr@hotmail.com>

--- RHMobi/Controller/CadastroViewController.swift
+++ RHMobi/Controller/CadastroViewController.swift
@@ -284,9 +284,10 @@ class CadastroViewController: UIViewController,UITextFieldDelegate {
         //Cadastrando...
         viewModel.cadastrar(id: 1, nome: self.nomeTextField.text!, sobrenome: self.sobreNomeTextField.text!, sexo: self.sexo, rg: self.rgTextField.text!, email: self.emailTextField.text!, senha: self.senhaTextField.text!, telefone1: self.telefoneTextField.text!, telefone2: self.telefone2TextField.text!)
         
+        self.navigationController?.popViewController(animated: true)
         
-        //Voltando para a tela anterior
-       self.navigationController?.popViewController(animated: true)
+        
+        
     }
     
     
