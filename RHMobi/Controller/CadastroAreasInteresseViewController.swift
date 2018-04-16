fef144169260988e2b142199084b9b1cb9cb3825
Edson Jr <eds_jr@hotmail.com>
2018-04-16 17:16:56 -0400

    [Cadastro] - A classe CadastroAreasInteresseViewController agora esta conformando com o protocolo da collectionView.
    Signed-off-by: Edson Jr <eds_jr@hotmail.com>

--- RHMobi/Controller/CadastroAreasInteresseViewController.swift
+++ RHMobi/Controller/CadastroAreasInteresseViewController.swift
@@ -10,12 +10,21 @@ import UIKit
 
 
 //View que contem uma collectionview para mostrar as areas de interesse do candidato
-class CadastroAreasInteresseViewController: UIViewController {
+class CadastroAreasInteresseViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate {
     
     
+    //Mark: Componentes da tela
     @IBOutlet weak var collectionView: UICollectionView!
     
+    //Mark: CollectionView datasource
+    var areasInteresseDataSource = [AreasInteresse]()
+
     private final let dbgmsg = "[CadastroAreasInteresseVC]: "
+    
+    
+    //Mark: Reuso da celula
+    let cellReuse = "collectionViewCell"
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -53,6 +62,30 @@ class CadastroAreasInteresseViewController: UIViewController {
         
     }
     
-
-
+    
+    //################## FUNCOES DE COLLECTIONVIEW #############
+    //Mark: Metodos dos protocolos da collectionview
+    
+    // tell the collection view how many cells to make
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return self.areasInteresseDataSource.count
+    }
+    
+    // make a cell for each cell index path
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        // get a reference to our storyboard cell
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: self.cellReuse, for: indexPath as IndexPath) as! AreasInteresseCollectionViewCell
+        
+        // Use the outlet in our custom class to get a reference to the UILabel in the cell
+        cell.nomeLabelAreaInteresse.text = self.areasInteresseDataSource[indexPath.item].nome
+        
+        return cell
+    }
+    
+    // MARK: - UICollectionViewDelegate protocol
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        // handle tap events
+        print("You selected cell #\(indexPath.item)!")
+    }
 }
